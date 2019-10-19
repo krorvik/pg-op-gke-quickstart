2fc79b681927efce09d0dd7306075e0de5a51058
Kenneth Rørvik <krorvik@gmail.com>
2019-10-19 15:48:12 +0200

    remove scripts

--- config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CUSTOMER="finn"
-ID="pgpoc"
-BILLINGACCOUNT="0183FE-3C29B7-21A890"
-PROJECT="$CUSTOMER-$ID"
-REGION="europe-north1"
-ZONE="europe-north1-a"
-CLUSTER="$PROJECT"
--- teardown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. ./config.sh
-
-gcloud -q projects delete $PROJECT
-# not needed as above will wipe it
-#gcloud -q container clusters delete $CLUSTER
-#gsutil rm -rf gs://$PROJECT 
-
