640f7ebb225cd78537baa5cc6d8ad09b8ed9bfcb
kenrorvi <kenneth.rorvik@finn.no>
2019-10-25 12:39:32 +0200

    wth, it works at current config!

--- teardown.sh
+++ teardown.sh
@@ -2,22 +2,23 @@
 
 PROJECT=$(cat .PROJECT | xargs)
 
-kubectl delete postgresql acid-minimal-cluster
-kubectl delete postgresql acid-restore-cluster
-kubectl delete statefulset acid-minimal-cluster
-kubectl delete statefulset acid-restore-cluster
-kubectl delete pvc pgdata-acid-minimal-cluster-0
-kubectl delete pvc pgdata-acid-restore-cluster-0
-kubectl delete service acid-minimal-cluster
-kubectl delete service acid-minimal-cluster-config
-kubectl delete service acid-minimal-cluster-repl
-kubectl delete service acid-restore-cluster
-kubectl delete service acid-restore-cluster-config
-kubectl delete service acid-restore-cluster-repl
-kubectl delete poddisruptionbudgets.policy postgres-acid-minimal-cluster-pdb
-kubectl delete poddisruptionbudgets.policy postgres-acid-restore-cluster-pdb
-kubectl delete -f operator-service-account-rbac.yaml
-kubectl delete -f postgres-pod-config.yaml
-kubectl delete -f postgres-operator.yaml
-kubectl delete -f postgresql-operator-default-configuration.yaml
+kubectl delete --grace-period 0 --force  postgresql acid-minimal-cluster
+kubectl delete --grace-period 0 --force  postgresql acid-restore-cluster
+kubectl delete --grace-period 0 --force  statefulset acid-minimal-cluster
+kubectl delete --grace-period 0 --force  statefulset acid-restore-cluster
+kubectl delete --grace-period 0 --force  pvc pgdata-acid-minimal-cluster-0
+kubectl delete --grace-period 0 --force  pvc pgdata-acid-minimal-cluster-0
+kubectl delete --grace-period 0 --force  pvc pgdata-acid-restore-cluster-0
+kubectl delete --grace-period 0 --force  service acid-minimal-cluster
+kubectl delete --grace-period 0 --force  service acid-minimal-cluster-config
+kubectl delete --grace-period 0 --force  service acid-minimal-cluster-repl
+kubectl delete --grace-period 0 --force  service acid-restore-cluster
+kubectl delete --grace-period 0 --force  service acid-restore-cluster-config
+kubectl delete --grace-period 0 --force  service acid-restore-cluster-repl
+kubectl delete --grace-period 0 --force  poddisruptionbudgets.policy postgres-acid-minimal-cluster-pdb
+kubectl delete --grace-period 0 --force  poddisruptionbudgets.policy postgres-acid-restore-cluster-pdb
+kubectl delete --grace-period 0 --force  -f operator-service-account-rbac.yaml
+kubectl delete --grace-period 0 --force  -f postgres-pod-config.yaml
+kubectl delete --grace-period 0 --force  -f postgres-operator.yaml
+kubectl delete --grace-period 0 --force  -f postgresql-operator-default-configuration.yaml
 gsutil rm -rf gs://$PROJECT/spilo/*
