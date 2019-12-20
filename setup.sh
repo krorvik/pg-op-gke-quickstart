1a7b6a7802c64ddef79e78407c2130adcaa31ad8
kenrorvi <kenneth.rorvik@finn.no>
2019-12-20 12:06:33 +0100

    adjustments made, but not all ready

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@
 PROJECT="$CUSTOMER-$ID"
 
 #Create with rw storage access, and a bit larger machine than usual so we con fit some pods
-gcloud container clusters create $PROJECT --zone=$ZONE --scopes=storage-rw --billing-project $PROJECT --machine-type n1-standard-2
+#gcloud container clusters create $PROJECT --zone=$ZONE --scopes=storage-rw --billing-project $PROJECT --machine-type n1-standard-2
 
 
 kubectl create -f manifests/operator-service-account-rbac.yaml
-cat << EOF | kubectl create -f -
+cat << EOF | kubectl apply -f -
 # This config map provides environment variables to the pods in postgresql clusters. They are used by the spilo image. 
 apiVersion: v1
 kind: ConfigMap
@@ -20,13 +20,13 @@ data:
   USE_WALG: "true"
   USE_WALG_BACKUP: "true"
   USE_WALG_RESTORE: "true"
-  #WALG_GS_BUCKET: gs://$PROJECT
+  WAL_GS_BUCKET: $PROJECT
   # We need to provide the bucket for WAL-G operations. DO NOT quote these vars - it breaks spilo configuration. 
-  WALG_GS_PREFIX: gs://$PROJECT/spilo/\$(SCOPE)
-  CLONE_WALG_GS_PREFIX: gs://$PROJECT/spilo/\$(CLONE_SCOPE)
+  #WALG_GS_PREFIX: gs://$PROJECT/spilo/\$(SCOPE)
+  #CLONE_WALG_GS_PREFIX: gs://$PROJECT/spilo/\$(CLONE_SCOPE)
   #This forces standby creation to always use pg_basebackup. 
-  WALE_BACKUP_THRESHOLD_PERCENTAGE: "100"   
+  WALE_BACKUP_THRESHOLD_PERCENTAGE: "100"
 EOF
-kubectl create -f manifests/postgres-operator.yaml
-sleep 20s # operator needs to be running before the below is run
-kubectl create -f manifests/postgresql-operator-default-configuration.yaml
+#kubectl create -f manifests/postgres-operator.yaml
+#sleep 20s # operator needs to be running before the below is run
+#kubectl create -f manifests/postgresql-operator-default-configuration.yaml
